9ee46ec401812e8be5a4d52b10277cb47ce3be8f
MisaelAlexander <alex.riva2022@gmail.com>
2025-05-21 19:03:01 -0600

    Inicio del PTC2025

---
+++ ArqosPTC/src/main/java/Arqos/ArqosPTC/ArqosPtcApplication.java
@@ -0,0 +1,13 @@
+package Arqos.ArqosPTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqosPtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqosPtcApplication.class, args);
+	}
+
+}
---
+++ ArqosPTC/src/test/java/Arqos/ArqosPTC/ArqosPtcApplicationTests.java
@@ -0,0 +1,13 @@
+package Arqos.ArqosPTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqosPtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
